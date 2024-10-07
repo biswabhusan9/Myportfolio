5a8b8be3ad027fcddc3dfae42e162175fee90ecf
biswabhusan9 <biswabhusanp442@gmail.com>
2024-10-07 12:00:50 +0530

    responsive for mobile

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,81 +1,84 @@
 import React from 'react'
-import { useTypewriter,Cursor } from 'react-simple-typewriter'
-import { FaFacebookF,FaTwitter,FaLinkedinIn,FaReact, FaInstagram } from 'react-icons/fa'
-import { SiTailwindcss,SiFigma,SiJavascript } from 'react-icons/si'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact, FaInstagram } from 'react-icons/fa'
+import { SiTailwindcss, SiFigma, SiJavascript } from 'react-icons/si'
 
 function LeftBanner() {
-    const [text]=useTypewriter({
-        words:["Software Engineer.","Software Developer","FrontEnd Developer."],
-        loop:true,
-        typeSpeed:35,
-        deleteSpeed:25,
-        delaySpeed:2000
+    const [text] = useTypewriter({
+        words: ["Software Engineer.", "Software Developer", "FrontEnd Developer."],
+        loop: true,
+        typeSpeed: 35,
+        deleteSpeed: 25,
+        delaySpeed: 2000
     })
-  return (
-    <div className='w-full lgl:w-1/2 flex flex-col gap-20'>
+
+    return (
+        <div className='w-full lgl:w-1/2 flex flex-col gap-10'>
             <div className='flex flex-col gap-5'>
-                <h1 className='text-6xl font-bold text-white'>
+                <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-white break-all'>
                     Hi, I'm {' '}
-                    <span className='text-designColor capitalize'>Biswabhusan Pradhan</span>
+                    <span className='text-designColor capitalize'>
+                        Biswabhusan Pradhan
+                    </span>
                 </h1>
-                <h2 className='text-4xl font-bold text-white'>
+                <h2 className='text-2xl sm:text-3xl md:text-4xl font-bold text-white'>
                     a <span>{text}</span>
                     <Cursor
-                    cursorBlinking="false"
-                    cursorStyle="|"
-                    cursorColor='#ff014f'
+                        cursorBlinking="false"
+                        cursorStyle="|"
+                        cursorColor='#ff014f'
                     />
                 </h2>
-                <p className='text-base font-bodyFont leading-6 tracking-wide'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                <p className='text-sm sm:text-base font-bodyFont leading-6 tracking-wide'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
             </div>
             <div className='flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between'>
                 <div>
-                <h2 className='text-base uppercase font-titleFont mb-4'>
-                    Find me in
-                </h2>
-                <div className='flex gap-4'>
-                <a
-        href="https://www.instagram.com/chikupradhan75570/?next=%2F&hl=en"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="bannerIcon"
-      >
-        <FaInstagram />
-      </a>
-                    <span className='bannerIcon'>
-                        <FaTwitter/>
-                    </span>
-                    <a
-        href="https://www.linkedin.com/in/biswabhusan-pradhan-187a15233/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="bannerIcon"
-      >
-        <FaLinkedinIn />
-      </a>
-                </div>
+                    <h2 className='text-base uppercase font-titleFont mb-4'>
+                        Find me in
+                    </h2>
+                    <div className='flex gap-4'>
+                        <a
+                            href="https://www.instagram.com/chikupradhan75570/?next=%2F&hl=en"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bannerIcon"
+                        >
+                            <FaInstagram />
+                        </a>
+                        <span className='bannerIcon'>
+                            <FaTwitter />
+                        </span>
+                        <a
+                            href="https://www.linkedin.com/in/biswabhusan-pradhan-187a15233/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bannerIcon"
+                        >
+                            <FaLinkedinIn />
+                        </a>
+                    </div>
                 </div>
                 <div>
-                <h2 className='text-base uppercase font-titleFont mb-4'>
-                    Best skill on
-                </h2>
-                <div className='flex gap-4'>
-                    <span className='bannerIcon'>
-                        <FaReact/>
-                    </span>
-                    <span className='bannerIcon'>
-                        <SiJavascript/>
-                    </span>
-                    <span className='bannerIcon'>
-                        <SiTailwindcss/>
-                    </span>
-                </div>
+                    <h2 className='text-base uppercase font-titleFont mb-4'>
+                        Best skill on
+                    </h2>
+                    <div className='flex gap-4'>
+                        <span className='bannerIcon'>
+                            <FaReact />
+                        </span>
+                        <span className='bannerIcon'>
+                            <SiJavascript />
+                        </span>
+                        <span className='bannerIcon'>
+                            <SiTailwindcss />
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div> 
-  )
+        </div>
+    )
 }
 
 export default LeftBanner
